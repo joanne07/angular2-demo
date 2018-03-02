b1ca0f1c9de8d6ee8ca267128b332fc6af053f7e
Apple <apple@AppledeMacBook-Pro.local>
2018-03-02 15:48:42 +0800

    first commit

---
+++ app/highlight.directive.ts
@@ -0,0 +1,12 @@
+//自定义指令
+import { Directive,ElementRef,Renderer} from '@angular/core';
+
+@Directive({
+    selector:'[highlight]',
+})
+
+export class HighlightDirective{
+    constructor(el:ElementRef,render:Renderer){
+        render.setElementStyle(el.nativeElement,'backgroundColor','yellow');
+    }
+}
---
+++ app/logger.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LoggerService {
+
+    constructor() { }
+
+    debug(msg:string){
+        console.log(msg);
+    }
+
+}
